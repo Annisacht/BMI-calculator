34e5cf7b704c0308e64397ecafb281d9cce7c5b2
Annisa Samanhudi <145771445+Annisacht@users.noreply.github.com>
2023-10-02 11:53:22 +0700

    nambahin js, sama ngatur css

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 
 function Calculate(){
-    var height = document.getElementById("h-input").value;
-    var weight = document.getElementById("w-input").value;
+    let height = document.getElementById("h-input").value;
+    let weight = document.getElementById("w-input").value;
 
-    var result = parseFloat(weight) /(parseFloat(height)/100)**2;
+    let result = parseFloat(weight) /(parseFloat(height)/100)**2;
+    let finalResult = result.toFixed(1)
 
-    if(!isNaN(result)){
-        document.getElementById("bmi-output").innerHTML = result;
-        if(result < 18.5){
+    if(!isNaN(finalResult)){
+        document.getElementById("bmi-output").innerHTML = finalResult;
+        if(finalResult < 18.5){
             document.getElementById("bmi-status").innerHTML = "Underweight";
         }
-        else if(result < 25){
+        else if(finalResult < 25){
             document.getElementById("bmi-status").innerHTML = "Normal";
         }
-        else if(result < 30){
+        else if(finalResult < 30){
             document.getElementById("bmi-status").innerHTML = "Overweight";
         }
         else{
             document.getElementById("bmi-status").innerHTML = "Obesity";
         }
     }
+
+    if ((height <= 0) || (weight <= 0)){
+        document.getElementById("bmi-status").innerHTML = "invalid";
+        document.getElementById("bmi-output").innerHTML = "Not Allowed 0";
+
+    }
+
 }
+
